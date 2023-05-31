a79a75fa9f7947550a31ca3ed1756c598a2a4a93
Berkant <berki9@icloud.com>
2023-05-31 20:22:05 +0300

    deleted all models for new models

--- src/main/java/com/example/ludogorieSoft/village/Repository/VillageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ludogorieSoft.village.Repository;
-import com.example.ludogorieSoft.village.Model.Village;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VillageRepository extends JpaRepository<Village, Long> {
-}
